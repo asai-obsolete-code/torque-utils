6274c73095c67746df1b20cadd588e4692d25bff
Masataro Asai <guicho2.71828@gmail.com>
2015-12-13 15:30:22 +0900

    prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,10 @@
 
 . $WORLD_HOME/template/common.sh
 
-sets="split1 split2 split3 split4 split5 split6 split7 split8 split9 split10 set1"
+# sets="split1 split2 split3 split4 split5 split6 split7 split8 split9 split10"
+# sets="set1"
+# sets="split1 split2 split10"
+sets="split3 split4 split5 split6 split7 split8 split9"
 
 expected_conf="test10"
 
