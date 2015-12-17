09ddff5492390f7e5439a6b9516549b1a43d385f
Masataro Asai <guicho2.71828@gmail.com>
2015-12-17 12:18:36 +0900

    prepare

--- prepare.sh
+++ prepare.sh
@@ -3,14 +3,37 @@
 . $WORLD_HOME/template/common.sh
 
 
-sets="set1"
+sets="allpath10"
 
-expected_conf="long"
+expected_conf="ipc"
 
 echo "#!/bin/bash"
 
+filter (){
+    echo "
+    {
+        tmp=\$(mktemp)
+        while read line ; do
+            [[ \$line ~= *.macro.$1.* ]] && echo \$line
+        done | sort -R > \$tmp
+        lines=\$(wc -l < \$tmp)
+        head -n \$((\$lines*$2/100)) < \$tmp
+        rm \$tmp
+    }
+"
+}
+
+# filter 5 10
+
 p=lama
 for s in $sets ; do
-    gen -s $s -n ${p} $base $macrocost $(blind 11) $nocost
-    echo "run $expected_conf results/$s-*-${p}-*"
+    for length in {2..10} ; do
+        for percent in $(seq 10 10 100)
+        do
+            gen -s $s -n ${p}-$length-$percent -f "$(filter $length $num)" $base $macrocos
+            echo "run $expected_conf results/$s-*-${p}-$length-$percent-*"
+        done
+    done
 done
+
+
