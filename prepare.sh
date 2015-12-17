e545f686b998c8870c617e2c3ee729fd96a0906e
Masataro Asai <guicho2.71828@gmail.com>
2015-12-17 12:24:03 +0900

    prepare.sh : remove macros explicitly

--- prepare.sh
+++ prepare.sh
@@ -9,7 +9,7 @@ expected_conf="ipc"
 
 echo "#!/bin/bash"
 
-filter (){
+filter-quoted (){
     echo "
     {
         tmp=\$(mktemp)
@@ -23,6 +23,16 @@ filter (){
 "
 }
 
+filter (){
+    tmp=$(mktemp)
+    while read line ; do
+        [[ $line ~= *.macro.$1.* ]] && echo $line
+    done | sort -R > $tmp
+    lines=$(wc -l < $tmp)
+    head -n $(($lines*$2/100)) < $tmp
+    rm $tmp
+}
+
 # filter 5 10
 
 p=lama
@@ -30,7 +40,7 @@ for s in $sets ; do
     for length in {2..10} ; do
         for percent in $(seq 10 10 100)
         do
-            gen -s $s -n ${p}-$length-$percent -f "$(filter $length $num)" $base $macrocos
+            gen -s $s -n ${p}-$length-$percent $base $macrocost
             echo "run $expected_conf results/$s-*-${p}-$length-$percent-*"
         done
     done
