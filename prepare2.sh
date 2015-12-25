7e2e82421949c9f1aa85374dea28b688e3e51b68
Masataro Asai <guicho2.71828@gmail.com>
2015-12-25 09:59:08 +0900

    re-fixed prepare2.sh, deleted old prepare.sh

--- prepare2.sh
+++ prepare2.sh
@@ -14,19 +14,21 @@ for s in $sets ; do
     length=${s##macros}
     for percent in 0 10 20 50 90
     do
-        gen -m $percent -s $s -n ${p}-$length-$percent $base $macrocost &
-        echo "run $expected_conf results/$s-*-${p}-$length-$percent-*"
+        name=${p}-$length-$percent-ns
+        gen -m $percent -s $s -n $name $base $macrocost &
+        echo "run $expected_conf results/$s-*-$name-*"
         for split in 1 2 3
         do
+            name=${p}-$length-$percent-$split
             (
-                gen -m $percent -s $s -n ${p}-$length-$percent-$split $base $macrocost
-                expdir=$(ls -d results/$s-*-${p}-$length-$percent-$split-*)
-                find sets/split$split -name "*.macro.*" | while read src ; do
-                    dest=$expdir${src##sets/split$split}
+                gen -m $percent -s $s -n $name $base $macrocost
+                expdir=$(ls -d results/$s-*-$name-*)
+                find sets/split3-$split -name "*.macro.*" | while read src ; do
+                    dest=$expdir${src##sets/split3-$split}
                     ln -s ../../../$src $dest
                 done
             ) &
-            echo "run $expected_conf results/$s-*-${p}-$length-$percent-$split-*"
+            echo "run $expected_conf results/$s-*-$name-*"
         done
     done
 done
