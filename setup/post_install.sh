cb957656cbf5f0873743ec95258ea3d76a05f175
Masataro Asai <guicho2.71828@gmail.com>
2016-05-25 17:48:42 +0900

    echodo

--- setup/post_install.sh
+++ setup/post_install.sh
@@ -7,38 +7,43 @@ wait-install (){
     done
 }
 
+echodo (){
+    echo $*
+    $*
+}
+
 (
-    apt-get install -q git
-    apt-get install -q emacs24-nox                                             # for everything
-    apt-get install -q libcurses-perl                                    # for pbstop
-    apt-get install -q build-essential automake make autoconf cmake      # for build
-    apt-get install -q libcurl4-openssl-dev                              # for roswell
-    apt-get install -q libtool libglib2.0-dev mercurial g++ python flex bison g++-multilib ia32-libs # for fd
-    apt-get install -q cgroup-bin libffi-dev                                                         # for CAP
-    apt-get install -q htop byobu
+    echodo apt-get install -q git
+    echodo apt-get install -q emacs24-nox                                             # for everything
+    echodo apt-get install -q libcurses-perl                                    # for pbstop
+    echodo apt-get install -q build-essential automake make autoconf cmake      # for build
+    echodo apt-get install -q libcurl4-openssl-dev                              # for roswell
+    echodo apt-get install -q libtool libglib2.0-dev mercurial g++ python flex bison g++-multilib ia32-libs # for fd
+    echodo apt-get install -q cgroup-bin libffi-dev                                                         # for CAP
+    echodo apt-get install -q htop byobu
 ) &> apt-get.log &
 
 (
     [ -d roswell/ ] || (
-        wait-install git
-        git clone -b release https://github.com/roswell/roswell.git
-        cd roswell
-        wait-install aclocal autoheader automake autoconf
-        ./bootstrap
-        ./configure
-        wait-install make
-        make
-        make install
+        echodo wait-install git
+        echodo git clone -b release https://github.com/roswell/roswell.git
+        echodo cd roswell
+        echodo wait-install aclocal autoheader automake autoconf
+        echodo ./bootstrap
+        echodo ./configure
+        echodo wait-install make
+        echodo make
+        echodo make install
     )
 ) &> roswell.log &
 
 (
     # torque setting
     PATH="/opt/torque/bin:$PATH"
-    qmgr -c "create node localhost"
-    qmgr -c "set node localhost np=10000"
-    pbsnodes -o localhost
-    qmgr -c "set queue batch keep_completed=0"
+    echodo qmgr -c "create node localhost"
+    echodo qmgr -c "set node localhost np=10000"
+    echodo pbsnodes -o localhost
+    echodo qmgr -c "set queue batch keep_completed=0"
     
 ) &> torque.log &
 
