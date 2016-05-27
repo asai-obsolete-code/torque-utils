ba934c07095911c74fcc6470dff3227270e64503
Masataro Asai <guicho2.71828@gmail.com>
2016-05-27 14:01:03 +0900

    debug [complete]

--- setup/post_install.sh
+++ setup/post_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+echo $_
+
 wait_install (){
     while ! which $@
     do
@@ -19,54 +21,54 @@ write_wasabi_once (){
     fi
 }
 
-# apt-get install -y git
-# apt-get install -y emacs24-nox                                  # for everything
-# apt-get install -y libcurses-perl                               # for pbstop
-# apt-get install -y build-essential automake make autoconf cmake # for build
-# apt-get install -y libcurl4-openssl-dev                         # for roswell
-# apt-get install -y libtool libglib2.0-dev mercurial g++ python flex bison g++-multilib # for fd
-# apt-get install -y cgroup-bin libffi-dev                                                         # for CAP
-# apt-get install -y htop byobu
-# 
-# [ -d roswell/ ] || git clone -b release https://github.com/roswell/roswell.git
-# 
-# (
-#     cd roswell
-#     wait_install aclocal autoheader automake autoconf
-#     ./bootstrap
-#     ./configure
-#     wait_install make gcc 
-#     make
-#     make install
-# )
-# 
-# # torque setting
-# 
-# /opt/torque/sbin/pbs_mom
-# /opt/torque/bin/qmgr -c "create node localhost"
-# /opt/torque/bin/qmgr -c "set node localhost np=1"
-# /opt/torque/bin/pbsnodes -o localhost
-# /opt/torque/bin/qmgr -c "set queue batch keep_completed=0"
-# 
-# chmod +x /opt/torque/contrib/pbstop
-# 
-# echo
-# 
-# write_wasabi_once /etc/profile <<EOF
-# export EDITOR="emacs"
-# export TZ="Asia/Tokyo"
-# EOF
-# 
-# write_wasabi_once /etc/sudoers <<EOF
-# Defaults	env_keep += "PATH EDITOR"
-# EOF
-# 
-# (
-#     su ubuntu
-#     write_wasabi_once /home/ubuntu/.profile <<EOF
-# export PATH=~/.roswell/bin:/opt/torque/contrib:/opt/torque/bin:/opt/torque/sbin:\$PATH
-# EOF
-# )
+apt-get install -y git
+apt-get install -y emacs24-nox                                  # for everything
+apt-get install -y libcurses-perl                               # for pbstop
+apt-get install -y build-essential automake make autoconf cmake # for build
+apt-get install -y libcurl4-openssl-dev                         # for roswell
+apt-get install -y libtool libglib2.0-dev mercurial g++ python flex bison g++-multilib # for fd
+apt-get install -y cgroup-bin libffi-dev                                                         # for CAP
+apt-get install -y htop byobu
+
+[ -d roswell/ ] || git clone -b release https://github.com/roswell/roswell.git
+
+(
+    cd roswell
+    wait_install aclocal autoheader automake autoconf
+    ./bootstrap
+    ./configure
+    wait_install make gcc 
+    make
+    make install
+)
+
+# torque setting
+
+/opt/torque/sbin/pbs_mom
+/opt/torque/bin/qmgr -c "create node localhost"
+/opt/torque/bin/qmgr -c "set node localhost np=1"
+/opt/torque/bin/pbsnodes -o localhost
+/opt/torque/bin/qmgr -c "set queue batch keep_completed=0"
+
+chmod +x /opt/torque/contrib/pbstop
+
+echo
+
+write_wasabi_once /etc/profile <<EOF
+export EDITOR="emacs"
+export TZ="Asia/Tokyo"
+EOF
+
+write_wasabi_once /etc/sudoers <<EOF
+Defaults	env_keep += "PATH EDITOR"
+EOF
+
+(
+    su ubuntu
+    write_wasabi_once /home/ubuntu/.profile <<EOF
+export PATH=~/.roswell/bin:/opt/torque/contrib:/opt/torque/bin:/opt/torque/sbin:\$PATH
+EOF
+)
 
 exit 0
 
