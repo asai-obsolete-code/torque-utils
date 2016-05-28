2a289a9d454c3eb015b4580eb70e0a342fa58735
Masataro Asai <guicho2.71828@gmail.com>
2016-05-28 10:07:27 +0900

    retry always

--- setup/post_install.sh
+++ setup/post_install.sh
@@ -43,11 +43,14 @@ echo roswell ; (
 
 echo torque ; (
     /opt/torque/sbin/pbs_mom
-    /opt/torque/bin/qmgr -c "create node localhost np=1,state=offline"
     /opt/torque/bin/qmgr -c "set server $(hostname) keep_completed=0,auto_node_np=false,allow_node_submit=true"
     # np_default=18
     # /opt/torque/bin/qmgr -c "set queue batch "
     chmod +x /opt/torque/contrib/pbstop
+
+    while : ; do
+        /opt/torque/bin/qmgr -c "create node localhost np=1,state=offline" && break
+    done
 ) &
 
 echo profile ; (
