9b20b9199f569f03240d3b0b7e1f36680ae229e6
Masataro Asai <guicho2.71828@gmail.com>
2016-11-13 19:10:50 +0900

    prepare

--- prepare.sh
+++ prepare.sh
@@ -21,7 +21,7 @@ bl1="blind(cost_type=one)"
 
 sets="ipc2011-sat ipc2014-sat"
 expected_conf="ipc4g"
-driver=cached-fd-clean3
+driver=cached-fd-clean5
 echo "#!/bin/bash"
 
 name (){
@@ -38,52 +38,78 @@ repeat (){
 }
 
 main (){
-    root=h; {
-        gen -s $s -r $root -n $(name) $base $plain --search $driver \
-            --random-seed $seed \
-            --heuristic "'h=$(ref $h)'" --search \
-            "'$search(single(h,queue_type=$q))'" -
-    }
-    root=hd; {
-        gen -s $s -r $root -n $(name) $base $plain --search $driver \
-            --random-seed $seed \
-            --heuristic "'h=$(ref $h)'" --search \
-            "'$search(typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q))'" -
-    }
-    root=hr ; {
-        gen -s $s -r $root -n $(name) $base $plain --search $driver \
-            --random-seed $seed \
-            --heuristic "'r=random()'" --heuristic "'h=$(ref $h)'" --search \
-            "'$search(tiebreaking([h,r],queue_type=$q))'" -
-    }
-    root=hb ; {
-        gen -s $s -r $root -n $(name) $base $plain --search $driver \
-            --random-seed $seed \
-            --heuristic "'h=$(ref $h)'" --search \
-            "'$search(tiebreaking([h,random_edge()],queue_type=$q))'" -
-    }
-    # root=hrd ; {
-    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
-    #         --heuristic "'r=random()'" --heuristic "'h=$(ref $h)'" --search \
-    #         "'$search(typed_tiebreaking([h,r],[depth([h])],stochastic=false,queue_type=$q))'" -
-    # }
-    # root=hdhr ; {
-    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
-    #         --heuristic "'r=random()'" --heuristic "'h=$(ref $h)'" --search \
-    #         "'$search(alt([tiebreaking([h,r],queue_type=$q),typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q)]))'" -
-    # }
-    # root=hR ; {
+    # root=h; {
     #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
     #         --random-seed $seed \
     #         --heuristic "'h=$(ref $h)'" --search \
-    #         "'iterated([$(repeat 5 "," "$search(tiebreaking([h,random(threshold=0.8)],queue_type=$q))")],repeat_last=true,continue_on_fail=true,continue_on_solve=false)'" -
+    #         "'$search(single(h,queue_type=$q))'" -
+    # }
+    # root=hw; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(typed_tiebreaking([h],[width()],stochastic=false,queue_type=$q))'" -
+    # }
+    # root=hW; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([width()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hp; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(typed_tiebreaking([h],[pwidth()],stochastic=false,queue_type=$q))'" -
+    # }
+    # root=hP; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([pwidth()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hdt; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q),type_based([h,g()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hdT; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q),type_based([h],queue_type=RANDOM)]))'" -
+    # }
+    # root=hb ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(tiebreaking([h,random_edge()],queue_type=$q))'" -
+    # }
+    # root=ht ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([h,g()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hT ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([h],queue_type=RANDOM)]))'" -
     # }
     # root=hB ; {
     #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
     #         --random-seed $seed \
     #         --heuristic "'h=$(ref $h)'" --search \
-    #         "'iterated([$(repeat 5 "," "$search(tiebreaking([h,random_edge(threshold=0.8)],queue_type=$q))")],repeat_last=true,continue_on_fail=true,continue_on_solve=false)'" -
+    #         "'$search(alt([single(h,queue_type=$q),single(random_edge(),queue_type=RANDOM)]))'" -
     # }
+    root=hbB ; {
+        gen -s $s -r $root -n $(name) $base $plain --search $driver \
+            --random-seed $seed \
+            --heuristic "'h=$(ref $h)'" --search \
+            "'$search(alt([tiebreaking([h,random_edge()],queue_type=$q),single(random_edge(),queue_type=RANDOM)]))'" -
+    }
 }
 
 first=true
@@ -93,14 +119,14 @@ main2 (){
     (
         for search in eager ; do
             for s in $sets ; do
-                for q in RANDOM ; do
-                    for h in cg1 ff1 bl1 ; do
-                        driver=cached-fd-clean3 main
+                for q in FIFO ; do
+                    for h in cg1 ff1 ; do
+                        main
                     done
                 done
             done
         done
-    ) > $(if $first ; then echo /dev/fd/1 ; else echo /dev/fd/2 ; fi)
+    ) >> $(if $first ; then echo /dev/fd/1 ; else echo /dev/fd/2 ; fi)
     first=false
 }
 
