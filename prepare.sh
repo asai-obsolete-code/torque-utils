a7d0cb234aeda098dba7d381d29df6798fe93ca2
Masataro Asai <guicho2.71828@gmail.com>
2017-04-06 17:07:58 +0900

    fractal test

--- prepare.sh
+++ prepare.sh
@@ -11,7 +11,6 @@ gc="goalcount()"
 cg="cg()"
 lc="lmcount(lm_rhw())"
 
-lm1="lmcut(cost_type=one)"
 ff1="ff(cost_type=one)"
 ad1="add(cost_type=one)"
 ce1="cea(cost_type=one)"
@@ -20,28 +19,105 @@ cg1="cg(cost_type=one)"
 lc1="lmcount(lm_rhw(),cost_type=one)"
 bl1="blind(cost_type=one)"
 
-sets="ipc2011-opt"
+sets="ipc2011-sat ipc2014-sat"
 expected_conf="ipc4g"
-
+driver=cached-fd-clean5
 echo "#!/bin/bash"
 
-# fig2
-for search in eager ; do
-    for s in $sets ; do
-        for h in lm1 ff1 ce1 ad1 cg1 bl1 gc1 ; do
-            name=${h}${search:0:1} ; root=fig2-base ; {
-		gen -s $s -r $root -n $name $base $plain \
-                    --search cached-fd-clean --heuristic "'h=$(ref $h)'" \
-                    --search "'$search(single(h))'" -
-		echo "run-unsolved $expected_conf $root/$s*-$name-*"
-	    }
-            name=${h}${search:0:1} ; root=fig2-macro ; {
-		gen -s $s -r $root -n $name $base --junk-type :reservoir --junk 2 :infinity \
-                    --search cached-fd-clean --heuristic "'h=$(ref $h)'" \
-                    --search "'$search(single(h))'" -
-		echo "run-unsolved $expected_conf $root/$s*-$name-*"
-	    }
-        done
+name (){
+    echo $h-$search-$q-$root
+}
+repeat (){
+    n=$1
+    delimiter=$2
+    echo -n $3
+    for ((i=0;i<n;i++))
+    do
+        echo -n $delimiter$3
     done
-done
+}
 
+main (){
+    # root=h; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(single(h,queue_type=$q))'" -
+    # }
+    root=hd; {
+        gen -s $s -r $root -n $(name) $base $plain --search $driver \
+            --random-seed $seed \
+            --heuristic "'h=$(ref $h)'" --search \
+            "'$search(typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q))'" -
+    }
+    # root=hdt; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q),type_based([h,g()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hdT; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q),type_based([h],queue_type=RANDOM)]))'" -
+    # }
+    root=hb ; {
+        gen -s $s -r $root -n $(name) $base $plain --search $driver \
+            --random-seed $seed \
+            --heuristic "'h=$(ref $h)'" --search \
+            "'$search(tiebreaking([h,random_edge()],queue_type=$q))'" -
+    }
+    # root=ht ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([h,g()],queue_type=RANDOM)]))'" -
+    # }
+    # root=hT ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),type_based([h],queue_type=RANDOM)]))'" -
+    # }
+    # root=hB ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([single(h,queue_type=$q),single(random_edge(),queue_type=RANDOM)]))'" -
+    # }
+    # root=hbB ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([tiebreaking([h,random_edge()],queue_type=$q),single(random_edge(),queue_type=RANDOM)]))'" -
+    # }
+    # root=hdbDB ; {
+    #     gen -s $s -r $root -n $(name) $base $plain --search $driver \
+    #         --random-seed $seed \
+    #         --heuristic "'h=$(ref $h)'" --search \
+    #         "'$search(alt([alt([tiebreaking([h,random_edge()],queue_type=$q),typed_tiebreaking([h],[depth([h])],stochastic=false,queue_type=$q)]),alt([single(random_edge(),queue_type=RANDOM),type_based([h,g()],queue_type=RANDOM)])]))'" -
+    # }
+}
+
+first=true
+
+main2 (){
+    seed=$RANDOM
+    (
+        for search in eager ; do
+            for s in $sets ; do
+                for q in FIFO ; do
+                    for h in ff1 ; do
+                        main
+                    done
+                done
+            done
+        done
+    ) >> $(if $first ; then echo /dev/fd/1 ; else echo /dev/fd/2 ; fi)
+    first=false
+}
+
+for i in {1..6} ; do
+    main2
+done
